c054807b6adfaf1e9713df02336f5dd9353f0a94
Patrice Vignola <vignola.patrice@gmail.com>
2021-10-07 15:08:42 -0700

    Add missing license headers

--- tfdml/core/plugin_kernel.cc
+++ tfdml/core/plugin_kernel.cc
@@ -1,4 +1,15 @@
-#include <iostream>
+/* Copyright (c) Microsoft Corporation.
+
+Use of this source code is governed by an MIT-style
+license that can be found in the LICENSE file or at
+https://opensource.org/licenses/MIT.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
 
 #include "tensorflow/c/kernels.h"
 
