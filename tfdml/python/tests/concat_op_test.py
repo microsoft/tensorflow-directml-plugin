aa00c923c520944252b6b34c25994bbe46e8fd0d
maggie1059 <34173352+maggie1059@users.noreply.github.com>
2021-10-22 13:00:03 -0700

    Ported concat op (#3)
    * Added concat op
    
    * updated concat op
    
    * disabled kernel cache
    
    Co-authored-by: Maggie Wu <wumaggie@microsoft.com>

--- tfdml/python/tests/concat_op_test.py
+++ tfdml/python/tests/concat_op_test.py
@@ -15,7 +15,8 @@
 """Functional tests for Concat Op."""
 
 import numpy as np
-
+import os
+# input(os.getpid())
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import errors_impl
@@ -432,53 +433,54 @@ class ConcatOpTest(test.TestCase):
                                                     output_shape)
     self.assertLess(err, 1e-11)
 
-  @test_util.run_deprecated_v1
-  def testIndexedSlicesConcatDim0Grad(self):
-    x_shapes = [[20, 7, 3], [10, 7, 3], [14, 7, 3]]
-    output_shape = [4, 7, 3]
-    x_vals = [
-        np.random.random_sample(x_shape).astype(np.float64)
-        for x_shape in x_shapes
-    ]
-    with self.cached_session():
-      xs = [constant_op.constant(x_val) for x_val in x_vals]
-      x_concat = array_ops.concat(xs, 0)
-      output = array_ops.gather(x_concat, [1, 2, 0, 5])
-      err = gradient_checker.compute_gradient_error(xs, x_shapes, output,
-                                                    output_shape)
-    self.assertLess(err, 1e-11)
+  # TODO: add back after uint64 support added (TF2 #36692608)
+  # @test_util.run_deprecated_v1
+  # def testIndexedSlicesConcatDim0Grad(self):
+  #   x_shapes = [[20, 7, 3], [10, 7, 3], [14, 7, 3]]
+  #   output_shape = [4, 7, 3]
+  #   x_vals = [
+  #       np.random.random_sample(x_shape).astype(np.float64)
+  #       for x_shape in x_shapes
+  #   ]
+  #   with self.cached_session():
+  #     xs = [constant_op.constant(x_val) for x_val in x_vals]
+  #     x_concat = array_ops.concat(xs, 0)
+  #     output = array_ops.gather(x_concat, [1, 2, 0, 5])
+  #     err = gradient_checker.compute_gradient_error(xs, x_shapes, output,
+  #                                                   output_shape)
+  #   self.assertLess(err, 1e-11)
 
-  @test_util.run_deprecated_v1
-  def testIndexedSlicesConcatDim1Grad(self):
-    x_shapes = [[20, 7, 3], [20, 3, 3], [20, 1, 3]]
-    output_shape = [4, 11, 3]
-    x_vals = [
-        np.random.random_sample(x_shape).astype(np.float64)
-        for x_shape in x_shapes
-    ]
-    with self.cached_session():
-      xs = [constant_op.constant(x_val) for x_val in x_vals]
-      x_concat = array_ops.concat(xs, 1)
-      output = array_ops.gather(x_concat, [1, 2, 0, 5])
-      err = gradient_checker.compute_gradient_error(xs, x_shapes, output,
-                                                    output_shape)
-    self.assertLess(err, 1e-11)
+  # @test_util.run_deprecated_v1
+  # def testIndexedSlicesConcatDim1Grad(self):
+  #   x_shapes = [[20, 7, 3], [20, 3, 3], [20, 1, 3]]
+  #   output_shape = [4, 11, 3]
+  #   x_vals = [
+  #       np.random.random_sample(x_shape).astype(np.float64)
+  #       for x_shape in x_shapes
+  #   ]
+  #   with self.cached_session():
+  #     xs = [constant_op.constant(x_val) for x_val in x_vals]
+  #     x_concat = array_ops.concat(xs, 1)
+  #     output = array_ops.gather(x_concat, [1, 2, 0, 5])
+  #     err = gradient_checker.compute_gradient_error(xs, x_shapes, output,
+  #                                                   output_shape)
+  #   self.assertLess(err, 1e-11)
 
-  @test_util.run_deprecated_v1
-  def testIndexedSlicesConcatDim2Grad(self):
-    x_shapes = [[20, 7, 3], [20, 7, 1], [20, 7, 2]]
-    output_shape = [4, 7, 6]
-    x_vals = [
-        np.random.random_sample(x_shape).astype(np.float64)
-        for x_shape in x_shapes
-    ]
-    with self.cached_session():
-      xs = [constant_op.constant(x_val) for x_val in x_vals]
-      x_concat = array_ops.concat(xs, 2)
-      output = array_ops.gather(x_concat, [1, 2, 0, 5])
-      err = gradient_checker.compute_gradient_error(xs, x_shapes, output,
-                                                    output_shape)
-    self.assertLess(err, 1e-11)
+  # @test_util.run_deprecated_v1
+  # def testIndexedSlicesConcatDim2Grad(self):
+  #   x_shapes = [[20, 7, 3], [20, 7, 1], [20, 7, 2]]
+  #   output_shape = [4, 7, 6]
+  #   x_vals = [
+  #       np.random.random_sample(x_shape).astype(np.float64)
+  #       for x_shape in x_shapes
+  #   ]
+  #   with self.cached_session():
+  #     xs = [constant_op.constant(x_val) for x_val in x_vals]
+  #     x_concat = array_ops.concat(xs, 2)
+  #     output = array_ops.gather(x_concat, [1, 2, 0, 5])
+  #     err = gradient_checker.compute_gradient_error(xs, x_shapes, output,
+  #                                                   output_shape)
+  #   self.assertLess(err, 1e-11)
 
   @test_util.run_deprecated_v1
   def testIndexedSlicesConcatDim1Grad_UnknownInputDim(self):
