f93f43599f3be112ebd39605e43793aff1845d51
Justin Stoecker <justoeck@microsoft.com>
2021-10-27 14:09:25 -0700

    bug comment

--- tfdml/core/common_runtime/dml/dml_device_cache.cc
+++ tfdml/core/common_runtime/dml/dml_device_cache.cc
@@ -179,13 +179,14 @@ static std::vector<DmlAdapter> FilterAdapters()
         return {};
     }
 
+    // TODO: Bug #36842799
     std::vector<DmlAdapter> adapters = EnumerateAdapters();
-    adapters.erase(
-        std::remove_if(
-            adapters.begin(),
-            adapters.end(),
-            [](const DmlAdapter& adapter) { return !Foo(adapter); }),
-        adapters.end());
+    // adapters.erase(
+    //     std::remove_if(
+    //         adapters.begin(),
+    //         adapters.end(),
+    //         [](const DmlAdapter& adapter) { return !Foo(adapter); }),
+    //     adapters.end());
 
     //            [](const DmlAdapter& adapter)
     //          { return !SupportsDmlDevice(adapter); }),
