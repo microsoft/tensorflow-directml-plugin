0d3b0e4abb71df7dc87bcce09711c9c8ffdfbc4c
Justin Stoecker <justoeck@microsoft.com>
2021-11-02 20:35:27 -0700

    extra code removal

--- tfdml/core/util/kernel_def_builder.h
+++ tfdml/core/util/kernel_def_builder.h
@@ -175,65 +175,6 @@ class KernelDefinition<
         }
     }
 
-    // Each op argument may contain 1 or more tensors, which isn't known until
-    // runtime. This function converts argument data to tensor data.
-    static NodeDef CreateNodeDef(TF_OpKernelConstruction* ctx)
-    {
-        TF_StringView name = TF_OpKernelConstruction_GetName(ctx);
-
-        NodeDef node = {};
-        node.op_name = std::string_view{name.data, name.len};
-        node.op_type_string = std::string_view{Op::name};
-
-        uint32_t arg_index = 0;
-        uint32_t tensor_index = 0;
-
-        uint32_t tensor_offsets[Op::argument_descs.size()];
-        uint32_t tensor_counts[Op::argument_descs.size()];
-
-        // Init all input tensors to DEVICE_MEMORY
-        for (; arg_index < Op::input_arg_count; arg_index++)
-        {
-            uint32_t arg_tensor_count = 1; // TODO: unless attr based
-            tensor_counts[arg_index] = arg_tensor_count;
-            tensor_offsets[arg_index] = tensor_index;
-            tensor_index += arg_tensor_count;
-        }
-        node.input_tensor_memory_types.resize(
-            tensor_index,
-            MemoryType::DEVICE_MEMORY);
-
-        // Init all output tensors to DEVICE_MEMORY
-        for (; arg_index < Op::input_arg_count + Op::output_arg_count;
-             arg_index++)
-        {
-            uint32_t arg_tensor_count = 1; // TODO: unless attr based
-            tensor_offsets[arg_index] = tensor_index;
-            tensor_counts[arg_index] = arg_tensor_count;
-            tensor_index += arg_tensor_count;
-        }
-        node.output_tensor_memory_types.resize(
-            tensor_index - node.input_tensor_memory_types.size(),
-            MemoryType::DEVICE_MEMORY);
-
-        // Set input/output tensors that live in HOST_MEMORY
-        for (auto arg : std::initializer_list<Op::Argument>{HostArguments...})
-        {
-            auto memory_types_start =
-                (GetArgumentType<Op>(arg) == ArgumentType::Input)
-                    ? node.input_tensor_memory_types.begin()
-                    : node.output_tensor_memory_types.begin();
-
-            uint32_t host_arg_index = ConvertOpDefEnumToIndex(arg);
-            std::fill_n(
-                memory_types_start + tensor_offsets[host_arg_index],
-                tensor_counts[host_arg_index],
-                MemoryType::HOST_MEMORY);
-        }
-
-        return node;
-    }
-
     static void* CreateKernel(TF_OpKernelConstruction* raw_ctx)
     {
         OpKernelConstruction ctx(raw_ctx);
