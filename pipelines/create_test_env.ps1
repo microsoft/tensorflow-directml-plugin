051387540f334425eb8982d5e8ef9680b0b6dc82
Justin Stoecker <justoeck@microsoft.com>
2021-12-16 12:46:06 -0800

    asdf

--- pipelines/create_test_env.ps1
+++ pipelines/create_test_env.ps1
@@ -2,27 +2,26 @@
 # Licensed under the MIT License.
 param
 (
-    [string]$StagingDirectory = $env:BUILD_STAGINGDIRECTORY,
-    [Parameter(Mandatory)][string]$ArtifactPath,
+    [string]$ArtifactsDirectory = $env:SYSTEM_ARTIFACTSDIRECTORY,
+    [Parameter(Mandatory)][string]$TestArtifactPath,
     [Parameter(Mandatory)][string]$TensorFlowPackage
 )
 
 $ErrorActionPreference = 'Stop'
 
-$InstallDir = Join-Path ($StagingDirectory | Resolve-Path) "miniconda3"
-$PluginPackage = (Get-ChildItem "$ArtifactPath/tensorflow_directml_plugin*.whl").FullName
+$InstallDir = Join-Path ($ArtifactsDirectory | Resolve-Path) "miniconda3"
+$PluginPackage = (Get-ChildItem "$TestArtifactPath/tensorflow_directml_plugin*.whl").FullName
+$TestEnvPath = "$ArtifactsDirectory/test_env"
+$TestArtifact = $TestArtifactPath | Split-Path -Leaf
+$PyVersionMajorDotMinor = $TestArtifact -replace '.*-cp(\d)(\d)', '$1.$2'
 
 Write-Host "Installing miniconda3 to $InstallDir"
 $Url = 'https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe'
-$DownloadPath = "$StagingDirectory/miniconda.exe"
+$DownloadPath = "$ArtifactsDirectory/miniconda.exe"
 (New-Object System.Net.WebClient).DownloadFile($Url, $DownloadPath)
 Start-Process $DownloadPath -ArgumentList '/NoRegistry=1', '/InstallationType=JustMe', '/RegisterPython=0', '/S', "/D=$InstallDir" -Wait
 & "$InstallDir/shell/condabin/conda-hook.ps1"
 
-$Artifact = $ArtifactPath | Split-Path -Leaf
-
-$PyVersionMajorDotMinor = $Artifact -replace '.*-cp(\d)(\d)', '$1.$2'
-$TestEnvPath = "$StagingDirectory/test_env"
 conda create --prefix $TestEnvPath python=$PyVersionMajorDotMinor -y
 conda activate $TestEnvPath
 pip install $TensorFlowPackage
--- pipelines/create_test_env.sh
+++ pipelines/create_test_env.sh
@@ -3,10 +3,21 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-miniconda_path="miniconda3"
+artifacts_path=$1
+test_artifact_path=$2
+tensorflow_package=$3
+
+install_dir="$artifacts_directory/miniconda3"
+plugin_package=$(ls $test_artifact_path/tensorflow_directml_plugin*.whl)
+test_env_path="$artifacts_directory/test_env"
+test_artifact=$(basename $test_artifact_path)
+py_version_major_dot_minor=$(echo $test_artifact | sed -E "s/.*-cp([0-9])([0-9])/\1.\2/")
+
+echo "Installing miniconda3 to $install_dir"
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -p $miniconda_path
-eval "$($miniconda_path/bin/conda shell.bash hook)" 
-conda create --name build python=$(vars.pyVersionMajorDotMinor) -y
-activate_cmd="source $(Build.StagingDirectory)/$miniconda_path/bin/activate build"
+bash Miniconda3-latest-Linux-x86_64.sh -b -p $install_dir
+eval "$($install_dir/bin/conda shell.bash hook)" 
+conda create --prefix $test_env_path python=$(vars.pyVersionMajorDotMinor) -y
+
+activate_cmd="source $install_dir/bin/activate $test_env_path"
 echo "##vso[task.setVariable variable=activateCommand;isOutput=true]$activate_cmd"
