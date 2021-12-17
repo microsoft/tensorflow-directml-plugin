c280af4d875e2e2130aa3bef7eb1d451b443ebb1
Justin Stoecker <justoeck@microsoft.com>
2021-12-16 16:22:21 -0800

    asdf

--- pipelines/create_test_summary_json.ps1
+++ pipelines/create_test_summary_json.ps1
@@ -3,16 +3,13 @@
 
 <#
 .SYNOPSIS
-Creates an aggregate of all test results.
+Creates a summary of all test groups across all test environments.
+
 .DESCRIPTION
 Each tests runs on multiple environments, where an environment is defined by the agent (hardware),
 build architecture, and build configuration. For example, a run may produce 4 builds 
 (e.g. x64.release, x64.debug, x86.release, x86.debug) that are tested by 8 agents for up to
 8*4 = 32 environments. If there are 2000 tests, then there may be up to 2000*32 = 64,000 results.
-This script will summarize test results across all environments so that each test has a single state:
-- passed: test case passes on all environments
-- failed: test case fails on at least one environment
-- skipped: test case skips on all environments (one failure/blocked promotes the test to failed/blocked)
 #>
 param
 (
@@ -20,79 +17,43 @@ param
     [string]$OutputPath = "$TestArtifactsPath\test_summary.json"
 )
 
-# Sort test results.
-$Summary = @{'groups'=@(); 'tests'=@{};}
-$TestGroupMap = @{}
-$AgentTestSummaryPaths = (Get-ChildItem "$TestArtifactsPath/*/*/summary.*.json").FullName
-foreach ($AgentTestSummaryPath in $AgentTestSummaryPaths)
+$Summary = @{}
+
+$TestMatrix = (Get-Content "$TestArtifactsPath/matrix.json" -Raw) | ConvertFrom-Json
+
+foreach ($Job in $TestMatrix)
 {
-    Write-Host "Parsing $AgentTestSummaryPath"
-    $AgentTestSummary = (Get-Content $AgentTestSummaryPath -Raw) | ConvertFrom-Json
-
-    $GroupName = ($AgentTestSummaryPath | Split-Path -Leaf) -replace 'summary\.(\w+)\.json', '$1'
-    $AgentName = $AgentTestSummaryPath | Split-Path -Parent | Split-Path -Parent | Split-Path -Leaf
-    $BuildName = $AgentTestSummaryPath | Split-Path -Parent | Split-Path -Leaf
-
-    if (!$TestGroupMap[$GroupName])
+    foreach ($TestConfig in $Job.agentTestConfigs)
     {
-        $TestGroupMap[$GroupName] = @{'Name'=$GroupName; 'Agents'=@(); 'Tests'=@{}}
-        $Summary.groups += $TestGroupMap[$GroupName]
-    }
+        $Build, $Group = $TestConfig.split(':')
+        $ResultsPath = "$TestArtifactsPath/$($Job.agentName)/$Build/summary.$Group.json"
 
-    $TestGroupMap[$GroupName].Agents +=
-    @{
-        'name'=$AgentName;
-        'build'=$BuildName;
-        'time_seconds'=$AgentTestSummary.time_seconds;
-        'counts'=
-        @{
-            'passed'=$AgentTestSummary.cases_passed; 
-            'failed'=$AgentTestSummary.cases_failed; 
-            'skipped'=$AgentTestSummary.cases_skipped; 
-        }
-    }
-
-    foreach ($ResultType in ('passed', 'failed', 'skipped'))
-    {
-        foreach ($TestCase in $AgentTestSummary["cases_${ResultType}"])
+        if (!$Summary.ContainsKey($Group))
         {
-            # Full test name format: <group>.<test>::<class>.<method>
-            # Example: plugin.profiler_test::ProfilerTest::testXPlaneKernelEvents
-            if (!$Summary.tests[$TestCase.name])
-            {
-                $Summary.tests[$TestCase.name] = @{
-                    'state'='?'
-                    'failed'=@(); 
-                    'passed'=@(); 
-                    'skipped'=@();
-                }
-            }
-    
-            $Summary.tests[$TestCase.name][$TestResult.Result] += "$AgentName!$BuildName";
+            $Summary[$Group] = @()
         }
-    }
-}
 
-# Determine each test's state.
-foreach ($TestSummary in $Summary.Tests.Values)
-{
-    $Total = $TestSummary.Fail.Count + $TestSummary.Pass.Count + $TestSummary.Blocked.Count + $TestSummary.Skipped.Count
+        $SummaryEntry = @{
+            "agent" = $Job.agentName;
+            "build" = $Build;
+            "agentWasOnline" = $Job.agentStatus -eq 'online';
+            "agentWasEnabled" = $Job.agentEnabled;
+            "agentHasResults" = Test-Path $ResultsPath;
+        }
 
-    if ($TestSummary.Fail.Count -gt 0)
-    {
-        $TestSummary.State = 'Fail'
-    }
-    elseif ($TestSummary.Blocked.Count -gt 0)
-    {
-        $TestSummary.State = 'Blocked'
-    }
-    elseif ($TestSummary.Skipped.Count -eq $Total)
-    {
-        $TestSummary.State = 'Skipped'
-    }
-    else
-    {
-        $TestSummary.State = 'Pass'
+        if (Test-Path $ResultsPath)
+        {
+            $EnvSummary = (Get-Content $ResultsPath -Raw) | ConvertFrom-Json
+
+            $SummaryEntry["cases_failed"] = $EnvSummary.cases_failed.count
+            $SummaryEntry["cases_skipped"] =  $EnvSummary.cases_skipped.count
+            $SummaryEntry["cases_passed"] = $EnvSummary.cases_passed.count
+            $SummaryEntry["cases_total"] = $SummaryEntry["cases_failed"] + $SummaryEntry["cases_skipped"] + $SummaryEntry["cases_passed"]
+            $SummaryEntry["tests_aborted"] = $EnvSummary.tests_timed_out.count
+            $SummaryEntry["tests_timed_out"] = $EnvSummary.tests_aborted.count
+        }
+        
+        $Summary[$Group] += $SummaryEntry
     }
 }
 
