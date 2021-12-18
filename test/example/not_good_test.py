7975acae1baadab70e102a836e72606ace0c70c7
Justin Stoecker <justoeck@microsoft.com>
2021-12-17 18:31:44 -0800

    asdf

--- test/example/not_good_test.py
+++
@@ -1,9 +0,0 @@
-import absl.testing.absltest as absltest
-import time
-
-class ThisTestFails(absltest.TestCase):
-    def testCaseA(self):
-        self.assertFalse(True)
-
-if __name__ == "__main__":
-    absltest.main()
--- test/example/slow_test.py
+++
@@ -1,9 +0,0 @@
-import absl.testing.absltest as absltest
-import time
-
-class SlowTest(absltest.TestCase):
-    def testSleep(self):
-        time.sleep(10)
-
-if __name__ == "__main__":
-    absltest.main()
