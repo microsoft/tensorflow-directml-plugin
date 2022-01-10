a7d9e37d337e37decca9e5867081f0dc7036aff6
Patrice Vignola <vignola.patrice@gmail.com>
2022-01-10 12:34:03 -0800

    Add Reduce Ops (#28)

--- tfdml/plugin/plugin_kernel.cc
+++ tfdml/plugin/plugin_kernel.cc
@@ -24,6 +24,7 @@ void RegisterKernels_Conv();
 void RegisterKernels_Gather();
 void RegisterKernels_GatherNd();
 void RegisterKernels_MatMul();
+void RegisterKernels_Reduce();
 void RegisterKernels_Resize();
 } // namespace tfdml
 
@@ -45,5 +46,6 @@ void TF_InitKernel()
     tfdml::RegisterKernels_Gather();
     tfdml::RegisterKernels_GatherNd();
     tfdml::RegisterKernels_MatMul();
+    tfdml::RegisterKernels_Reduce();
     tfdml::RegisterKernels_Resize();
 }
