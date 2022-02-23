d05a85ee833b4ec0e4c2600bc500c89f10077510
Patrice Vignola <vignola.patrice@gmail.com>
2022-02-23 15:46:07 -0800

    Add Cross

--- tfdml/plugin/plugin_kernel.cc
+++ tfdml/plugin/plugin_kernel.cc
@@ -27,6 +27,7 @@ void RegisterKernels_Conv();
 void RegisterKernels_CropAndResizeGradBoxes();
 void RegisterKernels_CropAndResizeGradImage();
 void RegisterKernels_CropAndResize();
+void RegisterKernels_Cross();
 void RegisterKernels_Cwise();
 void RegisterKernels_DataFormatDimMap();
 void RegisterKernels_DataFormatVecPermute();
@@ -88,6 +89,7 @@ void TF_InitKernel()
     tfdml::RegisterKernels_CropAndResizeGradBoxes();
     tfdml::RegisterKernels_CropAndResizeGradImage();
     tfdml::RegisterKernels_CropAndResize();
+    tfdml::RegisterKernels_Cross();
     tfdml::RegisterKernels_Cwise();
     tfdml::RegisterKernels_DataFormatDimMap();
     tfdml::RegisterKernels_DataFormatVecPermute();
