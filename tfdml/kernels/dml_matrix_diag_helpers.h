d4f42e1128f3609d5a1bf72207c1d4d19e111809
Maggie Wu <wumaggie@microsoft.com>
2022-03-02 00:15:07 -0800

    add test to json

--- tfdml/kernels/dml_matrix_diag_helpers.h
+++ tfdml/kernels/dml_matrix_diag_helpers.h
@@ -10,12 +10,18 @@ You may obtain a copy of the License at
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing perMatrixDiagPartmissions and
-limitations under the License.
+See the License for the specific language governing perMatrixDiagPartmissions
+and limitations under the License.
 ==============================================================================*/
 
-namespace dml {
-dml::Expression MatrixDiag(dml::Graph& scope, dml::Expression diag,
-                           int32_t k_min, int32_t k_max, float padding_value,
-                           int64_t out_height, int64_t out_width);
-}  // namespace dml
+namespace dml
+{
+dml::Expression MatrixDiag(
+    dml::Graph& scope,
+    dml::Expression diag,
+    int32_t k_min,
+    int32_t k_max,
+    float padding_value,
+    int64_t out_height,
+    int64_t out_width);
+} // namespace dml
