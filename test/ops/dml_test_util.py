b908e7d073758229aea4e266a39057db245cf2bd
Jonah <rjdyk@umich.edu>
2022-03-31 16:21:33 -0700

    Fixed header change and notice

--- test/ops/dml_test_util.py
+++ test/ops/dml_test_util.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # Copyright (c) Microsoft Corporation. All Rights Reserved.
 #
-# Use of this source code is governed by an MIT-style
-# license that can be found in the LICENSE file or at
-# https://opensource.org/licenses/MIT.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
