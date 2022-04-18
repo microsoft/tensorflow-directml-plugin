7eb4b1218a1a1e2fa67d17afae9ead25c45eddc3
Patrice Vignola <vignola.patrice@gmail.com>
2022-04-18 05:51:59 -0700

    WIP

--- test/ops/nn_fused_batchnorm_test.py
+++ test/ops/nn_fused_batchnorm_test.py
@@ -30,8 +30,7 @@ from tensorflow.python.ops import nn_grad
 from tensorflow.python.ops import nn_impl
 from tensorflow.python.ops import nn_ops
 from tensorflow.python.platform import test
-import os
-input(os.getpid())
+
 
 class BatchNormalizationTest(test.TestCase):
 
