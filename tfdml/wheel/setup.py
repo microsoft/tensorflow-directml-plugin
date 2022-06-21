b33c060ef5ad434ee49e0060624a038b0d4d18ac
Maggie Wu <wumaggie@microsoft.com>
2022-06-20 21:20:15 -0700

    add known issues and add package description

--- tfdml/wheel/setup.py
+++ tfdml/wheel/setup.py
@@ -1,5 +1,15 @@
-'''
-setup.py file to build wheel for tensorflow-directml-plugin
+'''TensorFlow-DirectML-Plugin is an open-source DirectML backend for TensorFlow.
+Accelerate machine learning training with TensorFlow on Windows and Windows
+Subsystem for Linux (WSL).
+
+TensorFlow-DirectML-Plugin leverages TensorFlow's Pluggable Device API to create
+a DirectML backend for hardware-accelerated machine learning workflows on any
+DirectX 12 compatible GPU.
+
+Install the base tensorflow and tensorflow-directml-plugin packages to get started.
+Check out the GitHub repository to try the samples or build from source.
+
+TensorFlow, the TensorFlow logo, and any related marks are trademarks of Google Inc.
 '''
 
 from __future__ import absolute_import
