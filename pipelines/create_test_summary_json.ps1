68fdc95cbf45233f24cfd4b75712d1fdd7a0133a
Patrice Vignola <vignola.patrice@gmail.com>
2022-07-07 17:17:22 -0700

    WIP

--- pipelines/create_test_summary_json.ps1
+++ pipelines/create_test_summary_json.ps1
@@ -35,7 +35,7 @@ foreach ($Job in $TestMatrix)
 
         $SummaryEntry = @{
             "agent" = $Job.agentName;
-            "build" = ${Build}-${TensorflowPackage};
+            "build" = "${Build}-${TensorflowPackage}";
             "agentWasOnline" = $Job.agentStatus -eq 'online';
             "agentWasEnabled" = $Job.agentEnabled;
             "agentHasResults" = Test-Path $ResultsPath;
