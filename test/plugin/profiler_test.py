c17593fb13d5d74a98b3fa05834a4c1e1563c184
Patrice Vignola <vignola.patrice@gmail.com>
2022-07-24 19:00:30 -0700

    WIP

--- test/plugin/profiler_test.py
+++ test/plugin/profiler_test.py
@@ -15,35 +15,39 @@
 # limitations under the License.
 # ==============================================================================
 
-import absl.testing.absltest as absltest
-import tensorflow as tf
-import tensorflow.core.profiler.protobuf.xplane_pb2 as xplane_pb2
-import tensorboard_plugin_profile.protobuf.tf_stats_pb2 as tf_stats_pb2
+"""Contains the tests for the DML profiler"""
+
 import tempfile
 import shutil
-from pathlib import Path
 import gzip
 import json
+from pathlib import Path
+from absl.testing import absltest
+import tensorflow as tf
+from tensorflow.core.profiler.protobuf import xplane_pb2
+from tensorboard_plugin_profile.protobuf import tf_stats_pb2
 
 
 class ProfilerTest(absltest.TestCase):
+    """Contains the tests for the DML profiler"""
 
-    # Executes a trivial graph. The profiler output provides the input data for test methods below.
+    # Executes a trivial graph. The profiler output provides the input data for test
+    # methods below.
     @classmethod
     def setUpClass(cls):
         tf.compat.v1.disable_eager_execution()
 
-        a = tf.compat.v1.placeholder(dtype=tf.float32, shape=[3, 1])
+        a_placeholder = tf.compat.v1.placeholder(dtype=tf.float32, shape=[3, 1])
         a_values = [[1], [2], [3]]
 
-        b = tf.compat.v1.placeholder(dtype=tf.float32, shape=[3, 1])
+        b_placeholder = tf.compat.v1.placeholder(dtype=tf.float32, shape=[3, 1])
         b_values = [[4], [6], [11]]
 
-        c = tf.compat.v1.placeholder(dtype=tf.float32, shape=[1, 3])
+        c_placeholder = tf.compat.v1.placeholder(dtype=tf.float32, shape=[1, 3])
         c_values = [[2, 4, 5]]
 
-        y1 = tf.raw_ops.AddN(inputs=[a, b], name="MyAdd")
-        y2 = tf.raw_ops.MatMul(a=y1, b=c, name="MyMultiply")
+        add_n = tf.raw_ops.AddN(inputs=[a_placeholder, b_placeholder], name="MyAdd")
+        mat_mul = tf.raw_ops.MatMul(a=add_n, b=c_placeholder, name="MyMultiply")
 
         profiler_options = tf.profiler.experimental.ProfilerOptions(
             host_tracer_level=2, python_tracer_level=0, device_tracer_level=1
@@ -54,25 +58,34 @@ class ProfilerTest(absltest.TestCase):
         tf.profiler.experimental.start(
             logdir=cls.root_log_dir, options=profiler_options
         )
-        with tf.compat.v1.Session() as s:
-            print(s.run(y2, feed_dict={a: a_values, b: b_values, c: c_values}))
+        with tf.compat.v1.Session() as session:
+            print(
+                session.run(
+                    mat_mul,
+                    feed_dict={
+                        a_placeholder: a_values,
+                        b_placeholder: b_values,
+                        c_placeholder: c_values,
+                    },
+                )
+            )
         tf.profiler.experimental.stop()
 
     @classmethod
     def tearDownClass(cls):
         shutil.rmtree(cls.root_log_dir)
 
-    def getProfilerLogFilePath(self, glob):
+    def _get_profiler_log_file_path(self, glob):
         profiles_dir = Path(self.__class__.root_log_dir) / "plugins" / "profile"
         last_profile_dir = [p for p in profiles_dir.iterdir() if p.is_dir()][-1]
         return list(Path(last_profile_dir).glob(glob))[-1]
 
-    # Checks that xplane.pb contains expected kernel events.
-    def testXPlaneKernelEvents(self):
-        file_path = self.getProfilerLogFilePath("*xplane.pb")
+    def test_x_plane_kernel_events(self):
+        """Checks that xplane.pb contains expected kernel events"""
+        file_path = self._get_profiler_log_file_path("*xplane.pb")
         xspace = xplane_pb2.XSpace()
-        with open(file_path, "rb") as f:
-            xspace.ParseFromString(f.read())
+        with open(file_path, "rb") as file:
+            xspace.ParseFromString(file.read())
 
         for xplane in xspace.planes:
             if not xplane.name.startswith("/device:"):
@@ -80,7 +93,7 @@ class ProfilerTest(absltest.TestCase):
 
             # DML planes are prefixed /device:GPU so that they get picked up
             # by tensorflow_stats and trace_viewer.
-            self.assertRegex(xplane.name, "/device:GPU:\d \(DirectML\) - .*")
+            self.assertRegex(xplane.name, r"/device:GPU:\d \(DirectML\) - .*")
 
             # DML profiler only emits a single line right now (CPU-timeline kernels).
             self.assertEqual(len(xplane.lines), 1)
@@ -108,11 +121,11 @@ class ProfilerTest(absltest.TestCase):
                 xplane.stat_metadata[matmul_event.stats[0].metadata_id].name, "tf_op"
             )
 
-    # Checks that trace.json contains expected device kernel events.
-    def testTraceKernelEvents(self):
-        file_path = self.getProfilerLogFilePath("*trace.json.gz")
-        with gzip.open(file_path) as f:
-            json_trace = json.loads(f.read())
+    def test_trace_kernel_events(self):
+        """Checks that trace.json contains expected device kernel events"""
+        file_path = self._get_profiler_log_file_path("*trace.json.gz")
+        with gzip.open(file_path) as file:
+            json_trace = json.loads(file.read())
 
         pid_to_event = {}
         dml_kernel_events = 0
@@ -129,24 +142,24 @@ class ProfilerTest(absltest.TestCase):
                 if trace_event["name"] == "MatMul" or trace_event["name"] == "AddN":
                     proc_event = pid_to_event[trace_event["pid"]]
                     self.assertRegex(
-                        proc_event["args"]["name"], "/device:GPU:\d \(DirectML\) - .*"
+                        proc_event["args"]["name"], r"/device:GPU:\d \(DirectML\) - .*"
                     )
                     dml_kernel_events += 1
 
         self.assertEqual(dml_kernel_events, 2)
 
-    # Checks that device kernels appear in the tensorflow_stats.pb
-    def testTensorFlowStats(self):
-        file_path = self.getProfilerLogFilePath("*tensorflow_stats.pb")
+    def test_tensorflow_stats(self):
+        """Checks that device kernels appear in the tensorflow_stats.pb"""
+        file_path = self._get_profiler_log_file_path("*tensorflow_stats.pb")
         database = tf_stats_pb2.TfStatsDatabase()
-        with open(file_path, "rb") as f:
-            database.ParseFromString(f.read())
+        with open(file_path, "rb") as file:
+            database.ParseFromString(file.read())
 
         # Even though we're using DML devices we should see "GPU" here
         # because the XPlanes are prefixed '/device:GPU:# (DirectML) ...'
-        self.assertEqual(database.device_type, "GPU")
+        self.assertEqual(database.device_type, "GPU")  # pylint:disable=no-member
 
-        records = database.without_idle.tf_stats_record
+        records = database.without_idle.tf_stats_record  # pylint:disable=no-member
         self.assertEqual(len(records), 3)
 
         # Record ordering might change.
